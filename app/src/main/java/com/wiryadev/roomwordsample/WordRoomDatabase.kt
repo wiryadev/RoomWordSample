40a28933232bc0533572c450fa291016bb96d12e
wiryadev <abrarwiryawan@gmail.com>
2022-04-06 10:09:55 +0700

    populate the database

--- app/src/main/java/com/wiryadev/roomwordsample/WordRoomDatabase.kt
+++ app/src/main/java/com/wiryadev/roomwordsample/WordRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(
     entities = [
@@ -16,13 +19,45 @@ abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
 
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            word = Word("Android")
+            wordDao.insert(word)
+            word = Word("Room")
+            wordDao.insert(word)
+        }
+    }
+
     companion object {
         // Singleton prevents multiple instances of database
         // opening at the same time.
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope,
+        ): WordRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -32,6 +67,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
                         WordRoomDatabase::class.java,
                         "word_database"
                     )
+                    .addCallback(WordDatabaseCallback(scope))
                     .build()
                 INSTANCE = instance
                 // return instance
